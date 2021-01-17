c47a55712ddb4911550b59a71f5bca41c48bac07
Artiom O <ovchinnikov_aa@pik.ru>
2021-01-17 15:05:37 +0300

    structure reconfiguration

---
+++ apiclients/discord.go
@@ -0,0 +1,31 @@
+package apiclients
+
+import (
+	"discobot/utils"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"discobot/handlers"
+)
+
+func DiscordClient() {
+
+	dg, err := discordgo.New("Bot " + utils.GetEnvString("DISCORD_TOKEN"))
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(handlers.MessageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+}
